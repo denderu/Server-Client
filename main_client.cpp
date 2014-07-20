5e7df362b5481f118ebec4d27ac6d6bb7b6f3e29
denderu <cser@ngs.ru>
2014-07-20 18:50:57 +0700

    all source files have been added

---
+++ main_client.cpp
@@ -0,0 +1,60 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+#include "class_client.h"
+
+using namespace std;
+
+int main()
+{
+	int domain, type, protocol = 0;
+	unsigned short int port; long int ipaddr;
+	string message;
+	cout<<"Welcome to Client program!"<<endl;
+	//Создание клиента
+	Client client;
+	cout<<"Choose domain: 0-AF_UNIX      1-AF_INET"<<endl;
+	cin>>domain;
+	switch(domain)
+	{
+		case 0: domain = AF_UNIX; break;
+		case 1: domain = AF_INET; break;
+		default: break;
+	};
+	
+	cout<<"Choose type: 0-SOCK_STREAM	1-SOCK_DGRAM	2-SOCK_RAW"<<endl;
+	cin>>type;
+	switch(type)
+	{
+		case 0: type = SOCK_STREAM; break;
+		case 1: type = SOCK_DGRAM; break;
+		case 2: type = SOCK_RAW; break;
+		default: break;
+	};
+	client.CreateSocket(domain, type, protocol);
+	
+	//cout<<"Enter the IP address"<<endl;
+	cout<<"Enter the number of port"<<endl;
+	cin>>port;
+	client.SetAddr(port, INADDR_LOOPBACK);
+	client.Connect();
+	
+	//В главном цикле получаем сообщения с сервера и отдаем команды,
+	//если это quit или no- выходим из программы
+	do
+	{
+	  Receive(client);
+	  cout<<client.buffer<<endl;
+	  cin>>message;
+	  Send(client, message.c_str());
+	} while((message != "quit") && (message != "no"));
+
+	return 0;
+}
